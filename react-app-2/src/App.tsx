6f11134caff2c46071b84d01b851ad720aee29ba
Max Vassilev <maximvassilev01@gmail.com>
2025-02-10 18:03:01 +0200

    Modify App.tsx

--- react-app-2/src/App.tsx
+++ react-app-2/src/App.tsx
@@ -10,13 +10,13 @@ function App() {
       <div className="pattern"></div>
 
       <div className="wrapper">
+
         <header>
           <img src="./hero.png" alt="Hero banner" />
           <h1>Find Movies you'll enjoy without the hassle</h1>
         </header>
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-        <h1 className="text-white">{searchTerm}</h1>
 
       </div>
     </main>
